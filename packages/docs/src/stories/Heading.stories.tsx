e7db57fb12fa2c3f987a6603237a9d006b24d043
Islanilton <islanilton.rodrigues@gmail.com>
2023-08-02 21:55:23 -0300

    chore: create a Heading component

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from '@it-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+}
